0f3eb13fe9b35e16632592fed4c7118908d6887f
tkcedric <tkcedric2002@gmail.com>
2025-09-04 07:13:56 +0100

    pandoc fixes

--- build.sh
+++ build.sh
@@ -2,10 +2,23 @@
 # exit on error
 set -o errexit
 
-# 1. Installer les dépendances système (pandoc pour les PDF)
+# 1. Install system dependencies (pandoc and LaTeX)
 echo "--- Installing system dependencies ---"
-apt-get update && apt-get install -y pandoc texlive-xetex texlive-fonts-recommended texlive-lang-french
+apt-get update && apt-get install -y --no-install-recommends \
+    pandoc \
+    texlive-xetex \
+    texlive-fonts-recommended \
+    texlive-lang-french \
+    texlive-latex-extra \
+    texlive-latex-recommended \
+    lmodern
 
-# 2. Installer les paquets Python
+# 2. Install Python dependencies
 echo "--- Installing Python dependencies ---"
+pip install --upgrade pip
 pip install -r requirements.txt
+
+# 3. Verify pandoc installation
+echo "--- Verifying pandoc installation ---"
+pandoc --version
+which pandoc
